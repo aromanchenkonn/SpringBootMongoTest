4ca43d999d6768f766744726f7ae9007016b36a2
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-10-02 00:09:48 +0300

    add rest client from real server

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -1,7 +1,11 @@
 package de.springboot.service;
 
+import com.google.common.collect.ImmutableList;
+import de.springboot.model.SiteDto;
+import de.springboot.model.SitesDto;
 import de.springboot.model.StackoverflowWebsite;
 import de.springboot.persistence.StackOverflowWebsiteRepository;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,63 +13,34 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
 @Service
 public class StackoverflowService {
     @Autowired
     private StackOverflowWebsiteRepository repository;
-
-
-        private static List<StackoverflowWebsite> items = new ArrayList<>();
-//    static {
-//        items.add(
-//                new StackoverflowWebsite(
-//                        "#1"
-//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
-//                        , "http://www.google.com"
-//                        , "google"
-//                        , "google home page"));
-//
-//        items.add(
-//                new StackoverflowWebsite(
-//                        "#2"
-//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
-//                        , "http://www.yandex.ru"
-//                        , "yandex"
-//                        , "yandex home page"));
-//
-//        items.add(
-//                new StackoverflowWebsite(
-//                        "#3"
-//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
-//                        , "http://www.mail.ru"
-//                        , "mail.ru"
-//                        , "mail.ru home page"));
-//
-//        items.add(
-//                new StackoverflowWebsite(
-//                        "#4"
-//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
-//                        , "https://www.sputnik.ru/"
-//                        , "sputnik"
-//                        , "sputnik home page"));
-//
-//        items.add(
-//                new StackoverflowWebsite(
-//                        "#5"
-//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
-//                        , "https://www.rambler.ru/"
-//                        , "rambler"
-//                        , "rambler home page"));
-//
-//    }
-//
-//    @PostConstruct
-//    public void init(){
-//        repository.save(items);
-//    }
-
-
+    @Autowired
+    private StackExchangeClient stackExchangeClient;
     public List<StackoverflowWebsite> findAll() {
-        return repository.findAll();
+        return stackExchangeClient.getSites().stream()
+                .map(this::toStackoverflowWebsite)
+                .filter(this::ignoreMeta)
+                .collect(collectingAndThen(toList(), ImmutableList::copyOf));
+    }
+
+    private boolean ignoreMeta(@NonNull StackoverflowWebsite stackoverflowWebsite) {
+        return !stackoverflowWebsite.getId().contains("meta.");
+    }
+
+
+    private StackoverflowWebsite toStackoverflowWebsite(@NonNull SiteDto input){
+        return new StackoverflowWebsite(
+                  input.getSite_url().substring("https://".length(), input.getSite_url().length() - ".com".length())
+                , input.getFavicon_url()
+                , input.getSite_url()
+                , input.getName()
+                , input.getAudience()
+        );
     }
 }
---
+++ src/main/java/de/springboot/service/StackoverflowServiceTemp.java
@@ -0,0 +1,72 @@
+package de.springboot.service;
+
+import de.springboot.model.StackoverflowWebsite;
+import de.springboot.persistence.StackOverflowWebsiteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StackoverflowServiceTemp {
+    @Autowired
+    private StackOverflowWebsiteRepository repository;
+
+
+  //      private static List<StackoverflowWebsite> items = new ArrayList<>();
+//    static {
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#1"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "http://www.google.com"
+//                        , "google"
+//                        , "google home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#2"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "http://www.yandex.ru"
+//                        , "yandex"
+//                        , "yandex home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#3"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "http://www.mail.ru"
+//                        , "mail.ru"
+//                        , "mail.ru home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#4"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "https://www.sputnik.ru/"
+//                        , "sputnik"
+//                        , "sputnik home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#5"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "https://www.rambler.ru/"
+//                        , "rambler"
+//                        , "rambler home page"));
+//
+//    }
+//
+//    @PostConstruct
+//    public void init(){
+//        repository.save(items);
+//    }
+
+
+    public List<StackoverflowWebsite> findAll() {
+        return repository.findAll();
+    }
+
+
+
+}
