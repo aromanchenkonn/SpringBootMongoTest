83e7257960d02735a39806ab33359e04d245f417
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-10-01 22:42:17 +0300

    final release test and app

--- src/main/java/de/springboot/service/StackoverflowService.java
+++ src/main/java/de/springboot/service/StackoverflowService.java
@@ -13,6 +13,58 @@ import java.util.List;
 public class StackoverflowService {
     @Autowired
     private StackOverflowWebsiteRepository repository;
+
+
+        private static List<StackoverflowWebsite> items = new ArrayList<>();
+//    static {
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#1"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "http://www.google.com"
+//                        , "google"
+//                        , "google home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#2"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "http://www.yandex.ru"
+//                        , "yandex"
+//                        , "yandex home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#3"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "http://www.mail.ru"
+//                        , "mail.ru"
+//                        , "mail.ru home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#4"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "https://www.sputnik.ru/"
+//                        , "sputnik"
+//                        , "sputnik home page"));
+//
+//        items.add(
+//                new StackoverflowWebsite(
+//                        "#5"
+//                        , "https://www.santouka.co.jp/wp/wp-content/themes/santouka/img/home/icon-mark.png"
+//                        , "https://www.rambler.ru/"
+//                        , "rambler"
+//                        , "rambler home page"));
+//
+//    }
+//
+//    @PostConstruct
+//    public void init(){
+//        repository.save(items);
+//    }
+
+
     public List<StackoverflowWebsite> findAll() {
         return repository.findAll();
     }
